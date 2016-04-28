eca5f06d2440105af0d062e0803c08cbfc4bcdd1
Patrick Stadler <patrick.stadler@gmail.com>
2016-04-28 15:12:44 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ django-pipeline-compass-rubygem is a Compass compiler for django-pipeline using 
 
 setup(
     name='django-pipeline-compass-rubygem',
-    version='0.1.8',
+    version='0.1.9',
     description=description,
     long_description=open('README.rst').read(),
     author='Patrick Stadler',
